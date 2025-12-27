9256951ab6aef169df58c44981683b388490ec53
Ayan Tuladhar <75155513+ayantuladhar1@users.noreply.github.com>
2025-12-26 22:21:34 -0700

    Create print_statements.py

---
+++ print_statements.py
@@ -0,0 +1,11 @@
+#Separate Print Statements Using Delimter
+print("Hello World", 123)
+print("Hello World", 123, 234, sep='$')
+
+#Separate prints in multiple lines
+print("""Hello World!
+How is the World?""")
+
+#Using Escape Characters to add quotes inside string
+print("Hello World! 'World' is great")
+print("Hello World! \'World\' is great")
