4137e9bce9e7d66ff3aaf5e4935411e3072fcaf4
Ayan Tuladhar <75155513+ayantuladhar1@users.noreply.github.com>
2025-12-28 19:14:28 -0700

    Create array.py

---
+++ array.py
@@ -0,0 +1,10 @@
+x = "Hello World"
+#To print first element of list
+print (x[0])
+#Get first 5 characters, we use slicing (n-1)
+print (x[0:5])
+#Get last 5 characters from the list
+print (x[6:12])
+#To print the whole list ((0n) n=length of the list)
+print(x[:]) #By default it is [0:11]
+print(x[0:len(x)])
